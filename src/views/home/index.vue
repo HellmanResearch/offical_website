f6f94341513b61ac1e31c10c8c262acaf698ed9f
mingming.tang <mingmingtang@aliyun.com>
2022-01-27 14:31:15 +0800

    init

---
+++ src/views/home/index.vue
@@ -0,0 +1,106 @@
+<template>
+    <div style="background-color: black">
+        <div class="page flex-col" style="margin: 0 auto;">
+            <div class="box1 flex-col justify-between">
+                <div class="main1 flex-col">
+                    <div style="" class="group1 flex-col">
+                        <div class="wrap1 flex-row justify-between">
+                            <div class="group2 flex-col"></div>
+                            <span class="txt1">View</span>
+                        </div>
+                        <div class="wrap2 flex-col">
+                            <img
+                                    class="pic1"
+                                    referrerpolicy="no-referrer"
+                                    src="./assets/img/SketchPngf1a578d92637f33400c4ea90564b2e6833926cf4941dec10488cb28d10f8ba9d.png"
+                            />
+                        </div>
+                        <div class="wrap3 flex-col">
+                            <div class="outer1 flex-col">
+                                <div class="mod1 flex-row"><span class="txt2">Value</span></div>
+                                <div class="mod2 flex-row">
+                                    <span class="word1">Safety</span>
+                                    <span class="txt3">Good&nbsp;Reputation</span>
+                                    <span class="word2">High&nbsp;Quality&nbsp;Service</span>
+                                </div>
+                                <div class="mod3 flex-row justify-between">
+                                    <div class="mod4 flex-col">
+                  <span class="paragraph1">
+                    MPC&nbsp;private&nbsp;key&nbsp;management&nbsp;signature&nbsp;to&nbsp;ensure&nbsp;asset&nbsp;security
+                    <br/>
+                    Operator&nbsp;stability&nbsp;requires&nbsp;85%&nbsp;stability
+                  </span>
+                                    </div>
+                                    <div class="mod5 flex-col justify-center">
+                  <span class="word3">
+                    Rich&nbsp;experience&nbsp;in&nbsp;operation&nbsp;and&nbsp;maintenance,&nbsp;has&nbsp;participated&nbsp;in&nbsp;projects&nbsp;such&nbsp;as&nbsp;Filecoin,&nbsp;Solana,&nbsp;Conflux,&nbsp;etc.
+                  </span>
+                                    </div>
+                                    <div class="mod6 flex-col">
+                  <span class="infoBox1">
+                    Intelligent&nbsp;management&nbsp;of&nbsp;operation&nbsp;and&nbsp;maintenance&nbsp;services
+                    <br/>
+                    7*24&nbsp;hours&nbsp;operation&nbsp;and&nbsp;maintenance&nbsp;service
+                  </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <img
+                                    class="img1"
+                                    referrerpolicy="no-referrer"
+                                    src="./assets/img/SketchPng5d690e2d419694cc21fe16049443609372065aea5d27c582a1d91ff762f12cb0.png"
+                            />
+                        </div>
+                    </div>
+                    <span class="txt4">HELLMAN</span>
+                    <div class="group3 flex-col">
+          <span class="txt5">
+            Hellman&nbsp;is&nbsp;a&nbsp;Web3&nbsp;research&nbsp;institute&nbsp;focusing&nbsp;on&nbsp;DAO,&nbsp;NFT,&nbsp;ETH&nbsp;and&nbsp;Layer2,&nbsp;and&nbsp;is&nbsp;actively&nbsp;involved&nbsp;in&nbsp;Ethereum&nbsp;POS&nbsp;direction.We&nbsp;have&nbsp;experience&nbsp;in&nbsp;building&nbsp;Filecoin's&nbsp;mining&nbsp;pools&nbsp;and&nbsp;participate&nbsp;in&nbsp;the&nbsp;SSV&nbsp;network.
+          </span>
+                    </div>
+                    <img
+                            class="pic2"
+                            referrerpolicy="no-referrer"
+                            src="./assets/img/SketchPng2cf405b0b1176a4b0a487a297f18310547406df843a7a623bf8c077b096b6fc2.png"
+                    />
+                </div>
+                <div class="main2 flex-col">
+                    <div class="layer1 flex-col justify-center">
+                        <div class="outer2 flex-row justify-between">
+                            <a href="https://medium.com/@HellmanResearch" target="_blank">
+                                <img
+                                        class="icon1"
+                                        referrerpolicy="no-referrer"
+                                        src="./assets/img/SketchPngb358f0bc9316187bc46e6d32036fb6ccfba965a7cc8c9e4dcdab8a75fc11c0de.png"
+                                />
+                            </a>
+                            <a href="https://twitter.com/HellmanResearch" target="_blank">
+                                <img
+                                        class="label1"
+                                        referrerpolicy="no-referrer"
+                                        src="./assets/img/SketchPng8b88bc56c1f3ec00942e5c36db998ec612f98bdb61c082531da8c36420c9d5b2.png"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <img
+                            class="pic3"
+                            referrerpolicy="no-referrer"
+                            src="./assets/img/SketchPng685599507b7635eac4f92985d3aac001150ea06f7e43f53b7da6ee719300f18c.png"
+                    />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+                constants: {}
+            };
+        },
+        methods: {}
+    };
+</script>
+<style scoped lang="css" src="./assets/index.css"/>
