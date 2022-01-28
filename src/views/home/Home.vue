ec1cd67b4bd6d588d32fe2c6c985dc23ec5bbe74
mingming.tang <mingmingtang@aliyun.com>
2022-01-28 11:20:24 +0800

    update layout

---
+++ src/views/home/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="box1">
+        <div style="width: 80%">
+            <div>
+                <img src="./assets/img/SketchPngb187f466f75eca38a5ba53f47fb1a94fbc111b1badf36909a094dd556b659a05.png">
+            </div>
+            <div>
+                <span class="txt4">HELLMAN</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Home.vue"
+    }
+</script>
+
+<style scoped lang="css" src="./assets/home.css"/>
