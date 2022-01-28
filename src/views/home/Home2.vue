511dd7aa8ed14bbb2147c2bdd240c55445059275
mingming.tang <mingmingtang@aliyun.com>
2022-01-28 10:52:04 +0800

    update layout

--- src/views/home/Home2.vue
+++ src/views/home/Home2.vue
@@ -1,48 +1,73 @@
 <template>
     <div class="page_background">
-        <div style="border: 1px red solid; margin: 0 200px; display: flex; justify-content: space-between; align-items: center; height: 100px">
-            <img src="./assets/img/SketchPngb187f466f75eca38a5ba53f47fb1a94fbc111b1badf36909a094dd556b659a05.png">
-            <a target="_blank"
-               href="https://explorer.ssv.network/operators/e4edcb950ee9f4a4f89dde5e9b74b615c04f21f204884bc53ffe2bebbcea4b93">
-                <span class="txt1">View</span>
-            </a>
-        </div>
-        <div style="margin: 311px 200px 0px 200px">
-            <span class="txt4">HELLMAN</span>
-        </div>
-        <div style="margin: 121px 200px 0px 200px">
-          <p class="txt5">
-            Hellman&nbsp;is&nbsp;a&nbsp;Web3&nbsp;research&nbsp;institute&nbsp;focusing&nbsp;on&nbsp;DAO,&nbsp;NFT,&nbsp;ETH&nbsp;and&nbsp;Layer2,&nbsp;and&nbsp;is&nbsp;actively&nbsp;involved&nbsp;in&nbsp;Ethereum&nbsp;POS&nbsp;direction.We&nbsp;have&nbsp;experience&nbsp;in&nbsp;building&nbsp;Filecoin's&nbsp;mining&nbsp;pools&nbsp;and&nbsp;participate&nbsp;in&nbsp;the&nbsp;SSV&nbsp;network.
-          </p>
-        </div>
-        <div class="txt2" style="text-align: center; margin: 605px 200px 0px 200px">Value</div>
-        <div style="border: 1px red solid; margin: 300px 200px; display: flex; justify-content: space-around; align-items: center; height: 300px">
-            <div>
-                <h3 class="word1">Safety</h3>
-                <div>MPC private key management signature to ensure asset security
-                    Operator stability requires 85% stability
+        <div class="page1_background">
+            <div style="margin: 0 260px; display: flex; justify-content: space-between; align-items: center; height: 100px">
+                <img width="45px" src="./assets/img/logo.png">
+                <a target="_blank" style="text-decoration: none"
+                   href="https://explorer.ssv.network/operators/e4edcb950ee9f4a4f89dde5e9b74b615c04f21f204884bc53ffe2bebbcea4b93">
+                    <span class="txt1">View</span>
+                </a>
+            </div>
+            <div style="margin: 150px 260px 0px 260px">
+                <span class="txt4">HELLMAN</span>
+            </div>
+            <div style="margin: 80px 260px 0px 260px">
+                <p class="txt5">
+                    Hellman&nbsp;is&nbsp;a&nbsp;Web3&nbsp;research&nbsp;institute&nbsp;focusing&nbsp;on&nbsp;DAO,&nbsp;NFT,&nbsp;ETH&nbsp;and&nbsp;Layer2,&nbsp;and&nbsp;is&nbsp;actively&nbsp;involved&nbsp;in&nbsp;Ethereum&nbsp;POS&nbsp;direction.We&nbsp;have&nbsp;experience&nbsp;in&nbsp;building&nbsp;Filecoin's&nbsp;mining&nbsp;pools&nbsp;and&nbsp;participate&nbsp;in&nbsp;the&nbsp;SSV&nbsp;network.
+                </p>
+            </div>
+            <div class="txt2" style="text-align: center; margin: 405px 260px 0px 260px">Value</div>
+            <div style="margin: 100px 120px; display: flex; justify-content: space-around; align-items: center; height: 300px">
+                <div>
+                    <h3 class="word1">Safety</h3>
+                    <div class="value_item_box">
+                        <p class="value_item_txt">MPC private key management signature to ensure asset security
+                            <br>
+                            Operator stability requires 85% stability
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <h3 class="word1">Good Reputation</h3>
+                    <div class="value_item_box">
+                        <p class="value_item_txt">Rich experience in operation and maintenance, has participated in
+                            projects
+                            such as Filecoin, Solana, Conflux, etc.
+                        </p>
+                    </div>
+                </div>
+                <div>
+                    <h3 class="word1">High Quality Service</h3>
+                    <div class="value_item_box">
+                        <p class="value_item_txt">Intelligent management of operation and maintenance services
+                            <br>
+                            7*24 hours operation and maintenance service
+                        </p>
+                    </div>
                 </div>
             </div>
-            <div>
-                <h3 class="word1">Safety</h3>
-                <div>MPC private key management signature to ensure asset security
-                    Operator stability requires 85% stability
+            <div style="margin-top: 200px; background: rgba(0, 0, 0, 0.8); display: flex; justify-content: center; align-items: center; height: 70px">
+                <div>
+                    <a href="https://medium.com/@HellmanResearch" target="_blank" style="margin-right: 11px">
+                        <img
+                                width="34px"
+                                class="icon1"
+                                referrerpolicy="no-referrer"
+                                src="./assets/img/medium.png"
+                        />
+                    </a>
+                </div>
+                <div>
+                    <a href="https://twitter.com/HellmanResearch" target="_blank" style="margin-left: 11px">
+                        <img
+                                width="34px"
+                                class="label1"
+                                referrerpolicy="no-referrer"
+                                src="./assets/img/twitter.png"
+                        />
+                    </a>
                 </div>
             </div>
-            <div>
-                <h3 class="word1">Safety</h3>
-                <div>MPC private key management signature to ensure asset security
-                    Operator stability requires 85% stability
-                </div>
-            </div>
-        </div>
-        <div style="border: 1px red solid; display: flex; justify-content: center; align-items: center; height: 100px">
-            <a>
-                medium
-            </a>
-            <a>
-                medium
-            </a>
         </div>
     </div>
 </template>
