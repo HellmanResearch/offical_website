cdfe591089798e7e06bafb3dc91ce8e9312d7ab2
mingming.tang <mingmingtang@aliyun.com>
2022-09-06 17:04:54 +0800

    202209061704

--- src/views/home/Home2.vue
+++ src/views/home/Home2.vue
@@ -1,87 +1,91 @@
 <template>
-    <div class="page_background">
-        <div class="page1_background">
-            <div style="margin: 0 260px; display: flex; justify-content: space-between; align-items: center; height: 100px">
-                <img width="45px" src="./assets/img/logo.png">
-                <div style="width: 180px; display: flex; justify-content: space-between;">
-                    <a target="_blank" style="text-decoration: none"
-                       href="http://rewards-ssv.hellman.team/">
-                        <span class="txt1">Rewards</span>
-                    </a>
-                    <a target="_blank" style="text-decoration: none"
-                       href="https://explorer.ssv.network/operators/127">
-                        <span class="txt1">Node</span>
-                    </a>
-                </div>
-            </div>
-            <div style="margin: 150px 260px 0px 260px">
-                <span class="txt4">HELLMAN</span>
-            </div>
-            <div style="margin: 80px 260px 0px 260px">
-                <p class="txt5">
-                  Hellman is a Web 3 research institute covering Ethereum, Layer 2, NFT, DAO extensively, and is actively involved in the infrastructure development of POS projects. With rich experience in PoW and PoS pools, such as Filecoin and Swarm, Hellman is now actively engaged in the ssv.network community.
-                </p>
-            </div>
-            <div class="txt2" style="text-align: center; margin: 405px 260px 0px 260px">Value</div>
-            <div style="margin: 100px 120px; display: flex; justify-content: space-around; align-items: center; height: 300px">
-                <div>
-                    <h3 class="word1">Safety</h3>
-                    <div class="value_item_box">
-                        <p class="value_item_txt">MPC private key management signature to ensure asset security
-                            <br>
-                            Operator stability requires 85% stability
-                        </p>
-                    </div>
-                </div>
-                <div>
-                    <h3 class="word1">Good Reputation</h3>
-                    <div class="value_item_box">
-                        <p class="value_item_txt">Rich experience in operation and maintenance, has participated in
-                            projects
-                            such as Filecoin, Solana, Conflux, etc.
-                        </p>
-                    </div>
-                </div>
-                <div>
-                    <h3 class="word1">High Quality Service</h3>
-                    <div class="value_item_box">
-                        <p class="value_item_txt">Intelligent management of operation and maintenance services
-                            <br>
-                            7*24 hours operation and maintenance service
-                        </p>
-                    </div>
-                </div>
-            </div>
-            <div style="margin-top: 200px; background: rgba(0, 0, 0, 0.8); display: flex; justify-content: center; align-items: center; height: 70px">
-                <div>
-                    <a href="https://medium.com/@HellmanResearch" target="_blank" style="margin-right: 11px">
-                        <img
-                                width="34px"
-                                class="icon1"
-                                referrerpolicy="no-referrer"
-                                src="./assets/img/medium.png"
-                        />
-                    </a>
-                </div>
-                <div>
-                    <a href="https://twitter.com/HellmanResearch" target="_blank" style="margin-left: 11px">
-                        <img
-                                width="34px"
-                                class="label1"
-                                referrerpolicy="no-referrer"
-                                src="./assets/img/twitter.png"
-                        />
-                    </a>
-                </div>
-            </div>
+  <div class="page_background">
+    <div class="page1_background">
+      <div style="margin: 0 260px; display: flex; justify-content: space-between; align-items: center; height: 100px">
+        <img width="45px" src="./assets/img/logo.png">
+        <div style="width: 180px; display: flex; justify-content: space-between;">
+          <a target="_blank" style="text-decoration: none"
+             href="http://rewards-ssv.hellman.team/">
+            <span class="txt1">Rewards</span>
+          </a>
+          <a target="_blank" style="text-decoration: none"
+             href="https://explorer.ssv.network/operators/127">
+            <span class="txt1">Node</span>
+          </a>
         </div>
+      </div>
+      <div style="margin: 150px 260px 0px 260px">
+        <span class="txt4">HELLMAN</span>
+      </div>
+      <div style="margin: 80px 260px 0px 260px">
+        <p class="txt5">
+          Hellman is a Web 3 research institute covering Ethereum, Layer 2, NFT, DAO extensively, and is actively
+          involved in the infrastructure development of POS projects. With rich experience in PoW and PoS pools, such as
+          Filecoin and Swarm, Hellman is now actively engaged in the ssv.network community.
+        </p>
+      </div>
+      <div class="txt2" style="text-align: center; margin: 405px 260px 0px 260px">Value</div>
+      <div
+          style="margin: 100px 120px; display: flex; justify-content: space-around; align-items: center; height: 300px">
+        <div>
+          <h3 class="word1">Safety</h3>
+          <div class="value_item_box">
+            <p class="value_item_txt">MPC private key management signature to ensure asset security
+              <br>
+              Operator stability requires 85% stability
+            </p>
+          </div>
+        </div>
+        <div>
+          <h3 class="word1">Quality</h3>
+          <div class="value_item_box">
+            <p class="value_item_txt">Intelligent management of operation and maintenance services
+              <br>
+              7*24 hours operation and maintenance service
+            </p>
+          </div>
+        </div>
+        <div>
+          <h3 class="word1">Reputation</h3>
+          <div class="value_item_box">
+            <p class="value_item_txt">Rich experience in operation and maintenance, has participated in
+              projects
+              such as Filecoin, Solana, Dfinity, Swarm, Conflux, SSV, Aptos etc.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div
+          style="margin-top: 200px; background: rgba(0, 0, 0, 0.8); display: flex; justify-content: center; align-items: center; height: 70px">
+        <div>
+          <a href="https://medium.com/@HellmanResearch" target="_blank" style="margin-right: 11px">
+            <img
+                width="34px"
+                class="icon1"
+                referrerpolicy="no-referrer"
+                src="./assets/img/medium.png"
+            />
+          </a>
+        </div>
+        <div>
+          <a href="https://twitter.com/HellmanResearch" target="_blank" style="margin-left: 11px">
+            <img
+                width="34px"
+                class="label1"
+                referrerpolicy="no-referrer"
+                src="./assets/img/twitter.png"
+            />
+          </a>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home2"
-    }
+export default {
+  name: "Home2"
+}
 </script>
 
 <style scoped lang="css" src="./assets/home.css"/>
